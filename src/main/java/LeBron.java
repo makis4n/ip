cdaf7fc928f86d7088080ef8f8d0fc75ffe48c4c
KwongWaiKin <waikin901@gmail.com>
2025-08-20 19:58:14 +0800

    Added support for echo

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class LeBron {
     public static void main(String[] args) {
         System.out.println("Hello! I'm LeBron.\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+
+        sc.close();
     }
 }
