9d65ee83bc3c287648b441ddad1e811a6b56356d
KwongWaiKin <waikin901@gmail.com>
2025-08-21 10:43:59 +0800

    Added support for list

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class LeBron {
     public static void main(String[] args) {
         System.out.println("Hello! I'm LeBron.\nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
 
         while (true) {
             String userInput = sc.nextLine();
 
-            if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                System.out.println(userInput);
+            switch (userInput.toLowerCase()) {
+                case "list":
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.printf("%d. %s%n", i + 1, taskList.get(i));
+                    }
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    taskList.add(userInput);
+                    System.out.printf("Added: %s%n", userInput);
             }
         }
 
-        sc.close();
     }
 }
