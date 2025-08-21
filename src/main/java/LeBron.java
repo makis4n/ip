34ed026f456baff9e0fe920641c66363f7bd1aaf
KwongWaiKin <waikin901@gmail.com>
2025-08-21 14:16:10 +0800

    Added support for Deadline, Event and ToDo Tasks

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -10,7 +10,6 @@ public class LeBron {
 
         while (true) {
             String userInput = sc.nextLine();
-            Task t = new Task(userInput);
 
             if (userInput.equalsIgnoreCase("list")) {
                 for (int i = 0; i < taskList.size(); i++) {
@@ -23,14 +22,39 @@ public class LeBron {
             } else if (userInput.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
                 Task temp = taskList.get(taskNumber);
+
                 temp.markAsDone();
                 System.out.printf("Nice! I've marked this task as done:\n%s%n", temp);
             } else if (userInput.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
                 Task temp = taskList.get(taskNumber);
+
                 temp.markAsNotDone();
                 System.out.printf("OK, I've marked this task as not done yet:\n%s%n", temp);
+            } else if (userInput.startsWith("todo ")) {
+                String description = userInput.substring(5);
+                Task t = new ToDo(description);
+
+                taskList.add(t);
+                System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.%n", t, taskList.size());
+            } else if (userInput.startsWith("deadline ")) {
+                String description = userInput.substring(9, userInput.indexOf("/by") - 1);
+                String by = userInput.substring(userInput.indexOf("/by") + 4);
+                Task t = new Deadline(description, by);
+
+                taskList.add(t);
+                System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.%n", t, taskList.size());
+            } else if (userInput.startsWith("event ")) {
+                String description = userInput.substring(6, userInput.indexOf("/from") - 1);
+                String start = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") - 1);
+                String end = userInput.substring(userInput.indexOf("/to") + 4);
+                Task t = new Event(description, start, end);
+
+                taskList.add(t);
+                System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.%n", t, taskList.size());
             } else {
+                Task t = new Task(userInput);
+
                 taskList.add(t);
                 System.out.printf("Added: %s%n", userInput);
             }
