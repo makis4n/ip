f9af9aa57f7916088d6284272448c514a521ed50
KwongWaiKin <waikin901@gmail.com>
2025-09-08 21:28:10 +0800

    Refactor MarkCommand.java

--- src/main/java/lebron/command/MarkCommand.java
+++ src/main/java/lebron/command/MarkCommand.java
@@ -11,7 +11,7 @@ import lebron.task.TaskList;
  * When executed, it updates the specified task's status and displays a confirmation message.
  */
 public class MarkCommand extends Command {
-    private String arguments;
+    private final String arguments;
 
     /**
      * Constructor for MarkCommand.
@@ -28,16 +28,33 @@ public class MarkCommand extends Command {
 
     @Override
     public String execute(TaskList taskList, Ui ui, Storage storage) throws LeBronException {
+        Task taskToMark = getTaskIfValid(taskList);
+        taskToMark.markAsDone();
+        return markTaskMessage(taskToMark);
+    }
+
+    /**
+     * Parses and validates the task number from the command arguments.
+     * @param taskList The current task list.
+     * @return The task to be marked as done.
+     * @throws LeBronException If the task number is invalid or out of range.
+     */
+    private Task getTaskIfValid(TaskList taskList) throws LeBronException {
         try {
             int taskNumber = Integer.parseInt(arguments.trim()) - 1;
-            Task temp = taskList.getTasks().get(taskNumber);
-
-            temp.markAsDone();
-            return String.format("Nice! I've marked this task as done:\n%s%n", temp);
+            return taskList.getTask(taskNumber);
         } catch (IndexOutOfBoundsException e) {
             throw new LeBronException("Error: Task number out of range.");
         } catch (NumberFormatException e) {
             throw new LeBronException("Error: Please enter a valid task number.");
         }
+    }
+    /**
+     * Formats the message to be displayed after a task is marked as done.
+     * @param task The task that was marked as done.
+     * @return A formatted string message.
+     */
+    private String markTaskMessage(Task task) {
+        return String.format("Nice! I've marked this task as done:\n%s%n", task);
     }
 }
