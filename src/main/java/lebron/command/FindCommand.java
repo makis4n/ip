2d8a50574fba88cd5cc660ee0e18c1d3b7ee103c
KwongWaiKin <waikin901@gmail.com>
2025-09-09 15:04:23 +0800

    Fix mistake in merge commit #2

--- src/main/java/lebron/command/FindCommand.java
+++ src/main/java/lebron/command/FindCommand.java
@@ -69,6 +69,5 @@ public class FindCommand extends Command {
                     .collect(Collectors.joining("\n"));
             return result;
         }
-        return foundTasks.toString();
     }
 }
